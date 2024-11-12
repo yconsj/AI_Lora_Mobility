34c3602f8fda83c4dc423e1ed892350d5d0a148b
ycons <yconsj@gmail.com>
2024-11-12 13:26:37 +0100

    update

--- python_scripts/baselines3/simpleenvrunner.py
+++ python_scripts/baselines3/simpleenvrunner.py
@@ -1,16 +1,14 @@
-from stable_baselines3 import PPO
+from stable_baselines3 import DQN, PPO
 from simple_env import SimpleBaseEnv
 from simple_env import RewardPlottingCallback
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.env_util import make_vec_env
-env = SimpleBaseEnv()
-# Define and Train the agent
-check_env(env, warn=True)
 
 
 
 vec_env = make_vec_env(SimpleBaseEnv, n_envs=1, env_kwargs=dict())
-model = PPO("MlpPolicy", env).learn(50000)
+model = PPO("MlpPolicy", vec_env, ent_coef=0.05).learn(200000, callback=RewardPlottingCallback())
 
 model.save("stable-model")
 
+obs = vec_env.reset()
