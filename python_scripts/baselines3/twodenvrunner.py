a88f87892fab8782479cdfcecfd6a283046dd850
AugustV <augustvalentin00@gmail.com>
2025-01-30 21:14:42 +0100

    programmatic scaling of eval freq

--- python_scripts/baselines3/twodenvrunner.py
+++ python_scripts/baselines3/twodenvrunner.py
@@ -1,3 +1,4 @@
+import math
 import multiprocessing
 import warnings
 
@@ -10,6 +11,7 @@ from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from stable_baselines3.common.vec_env import SubprocVecEnv
 
 from twod_env import TwoDEnv, FrameSkip
+
 
 # Set TensorFlow logging to errors only (ignores warnings and info)
 tf.get_logger().setLevel('ERROR')
@@ -60,9 +62,10 @@ class CustomPolicyNetwork(BaseFeaturesExtractor):
         x = self.output_layer(x)
         return x
 
-
+# tensorboard info
 ## tensorboard --logdir ./tensorboard/; ##
 # http://localhost:6006/
+
 class TensorboardCallback(BaseCallback):
     """
     Custom callback for plotting additional values in tensorboard, only during evaluation.
@@ -73,7 +76,6 @@ class TensorboardCallback(BaseCallback):
 
     def _on_step(self) -> bool:
         # Ensure this callback is only triggered during evaluation
-
         infos = self.locals['infos']
         dones = self.locals["dones"]
         for i, done in enumerate(dones):
@@ -90,40 +92,52 @@ class TensorboardCallback(BaseCallback):
                 self.logger.record("custom_logs/total_received", total_received_values)
                 self.logger.record("custom_logs/total_misses", total_misses_values)
                 self.logger.record("custom_logs/delivery_rate", packet_delivery_rate)
+        self.logger.dump(self.num_timesteps)
         return True
 
 
 def main():
-
-    envs = 16
+    model_class = PPO  # (or DQN)
+    if model_class == PPO:
+        envs = 16
+    else:  # assuming DQN.
+        # TODO: below needs to be verified:
+        #  'Off-policy do not make good use of multiple environments at once.'
+        envs = 1
     env = make_vec_env(make_skipped_env, n_envs=envs, vec_env_cls=SubprocVecEnv)
     gamma = 0.85  # base: 0.85
     ent_coef = 0.005  # base: 0.005
     learning_rate = 1e-4  # base: 6e-5
-
-    n_blocks = 3  # # base: 2
+    n_blocks = 0
+    max_steps = env.get_attr("max_steps")[0]  # env_id=0 in VecEnv
+    time_skip = env.get_attr("_skip")[0]
+    steps_per_episode = math.ceil(max_steps / time_skip)
+    print(f"{max_steps = }")
+    print(f"{time_skip = }")
 
     stop_train_callback = StopTrainingOnNoModelImprovement(max_no_improvement_evals=50, min_evals=100, verbose=1)
-    eval_callback = EvalCallback(env, eval_freq=86400 / 40, callback_after_eval=stop_train_callback,
+    eval_callback = EvalCallback(env, eval_freq=steps_per_episode * 2, callback_after_eval=stop_train_callback,
                                  verbose=1, best_model_save_path="stable-model-2d-best")
-
-    policy_kwargs = dict(
-        #features_extractor_class=CustomPolicyNetwork,
-        #features_extractor_kwargs=dict(features_dim=64, num_blocks=n_blocks),
-        #activation_fn=nn.ReLU,
-        share_features_extractor=True,
-        net_arch=[64, 64, 64]
-    )
-    if True:
+    if model_class == PPO:
+        policy_kwargs = dict(
+            # features_extractor_class=CustomPolicyNetwork,
+            # features_extractor_kwargs=dict(features_dim=64, num_blocks=n_blocks),
+            # activation_fn=nn.ReLU,
+            share_features_extractor=True,
+            net_arch=[64, 64, 64]
+        )
         model = PPO("MlpPolicy", env, device="cpu", learning_rate=learning_rate, gamma=gamma, ent_coef=ent_coef,
-                    batch_size=64,  # base: 64
-                    clip_range=0.15,  # base: 0.15
-                    n_steps=4096,  # one episode is roughly 4000 steps, when using time_skip=10 # base: 4096
-                    n_epochs=10,  # base: 10
+                    batch_size=64,
+                    clip_range=0.15,
+                    n_steps=steps_per_episode * 2,
+                    n_epochs=10,
                     policy_kwargs=policy_kwargs,
                     tensorboard_log="./tensorboard/",
                     )
-    else:
+    else:  # assuming DQN
+        policy_kwargs = dict(
+            net_arch=[64, 64, 64]
+        )
         model = DQN("MlpPolicy", env, device="cpu", learning_rate=learning_rate, gamma=gamma,
                     batch_size=64,  # base: 64
                     policy_kwargs=policy_kwargs,
